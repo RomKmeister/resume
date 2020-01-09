bd99aa7488d50774489a65b107972528712265c6
RomK <romank1988@mail.ru>
2020-01-09 13:09:22 +0300

    Добавлены настройки сборки проекта

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.pug$/,
+        use: ['html-loader?attrs=false', 'pug-html-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        include: /fonts/,
+        use: ['file-loader?name=fonts/[name].[ext]']
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        exclude: /fonts/,
+        use: ['file-loader?name=images/[name].[ext]']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+      template: 'base.scss'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.pug'
+    })
+  ]
+};
