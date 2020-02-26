9a3aa520c70326782f5d73950127cbc8454a2173
RomKmeister <romank1988@mail.ru>
2020-02-27 01:11:13 +0300

    ДОбавлена дефолтная иконка-заглушка #6

--- src/app.js
+++ src/app.js
@@ -4,4 +4,5 @@ function importAll (r) {
 
 importAll(require.context('../src/fonts', true, /\.*$/));
 importAll(require.context('../src/favicon', true, /\.*$/));
+importAll(require.context('../src/', true, /\.svg$/));
 importAll(require.context('../src/', true, /\.scss$/))
